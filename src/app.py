cbdb435d30e1640196cd581456a4119d16c5da43
kabrick <kabuyedouglas53@gmail.com>
2020-04-13 17:16:04 +0300

    complete functionality for the post method

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
-import flask
+from flask import Flask, request, jsonify
+import estimator
+import json
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+@app.route('/', methods=['POST'])
+def receive_data():
+    return jsonify(estimator.estimator(json.loads(request.form.get("data"))))
 
 app.run()
