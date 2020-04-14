cd3a8d859f145e3705be46bae76bdfda6dd95561
kabrick <kabuyedouglas53@gmail.com>
2020-04-14 09:13:15 +0300

    edits

--- app.py
+++ app.py
@@ -32,10 +32,6 @@ def read_logged_data():
 
 @app.route('/api/v1/on-covid-19', methods=['POST'])
 def receive_data():
-    file1 = open("logger.txt", "a")
-    file1.write(str(request.form) +" \n")
-    file1.write(str(request.content_type) +" \n")
-    file1.close()
     if request.form.get("data") == None:
         return jsonify(estimator.estimator(request.json))
     else:
@@ -43,11 +39,17 @@ def receive_data():
 
 @app.route('/api/v1/on-covid-19/json', methods=['POST'])
 def receive_data_json():
-    return jsonify(estimator.estimator(json.loads(request.form.get("data"))))
+    if request.form.get("data") == None:
+        return jsonify(estimator.estimator(request.json))
+    else:
+        return jsonify(estimator.estimator(json.loads(request.form.get("data"))))
 
 @app.route('/api/v1/on-covid-19/xml', methods=['POST'])
 def receive_data_xml():
-    return Response(dicttoxml(estimator.estimator(json.loads(request.form.get("data")))), mimetype='text/xml')
+    if request.form.get("data") == None:
+        return Response(dicttoxml(estimator.estimator(request.json)), mimetype='text/xml')
+    else:
+        return Response(dicttoxml(estimator.estimator(json.loads(request.form.get("data")))), mimetype='text/xml')
 
 if __name__ == "__main__":
     app.run()
